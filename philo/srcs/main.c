f6fbd396ae5556a028fcbcd86ee16dd30577a9c8
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2026-01-04 01:39:50 -0300

    main function

---
+++ philo/srcs/main.c
@@ -0,0 +1,22 @@
+#include "philo.h"
+
+int	main(int argc, char **argv)
+{
+	t_data			data;
+	t_philo			*philos;
+	pthread_mutex_t	*forks;
+
+	if (argc < 5 || argc > 6)
+	{
+		printf("Use: %s num_philos time_to_die time_to_eat time_to_sleep ", argv[0]);
+		printf("[num_must_eat]\n");
+		return (1);
+	}
+	if (!parse_args(argc, argv, &data))
+		return (1);
+	if (!init_data(&data, &philos, &forks))
+		return (1);
+	create_threads(&data, philos);
+	cleanup(&data, philos, forks);
+	return (0);
+}
